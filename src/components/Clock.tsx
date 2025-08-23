9f276819077661dc7a24650604455b753ef90732
Ashan Ghimire <ashanghimire10@gmail.com>
2025-08-23 19:23:45 +0545

    remove seconds from digital clock

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -15,7 +15,7 @@ export const Clock: Component<ClockProps> = (props: ClockProps) => {
     return (
         <div class="relative flex flex-col w-full h-full bg-neutral-50 rounded-lg shadow-sm p-4 dark:bg-neutral-900 dark:text-neutral-300">
             <h1 class="text-teal-500 text-6xl" style="font-family: 'Bungee', sans-serif">
-                {props.date.toLocaleTimeString("en-US", { timeZone: local.timezone, hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: state.twelveHourFormat })}
+                {props.date.toLocaleTimeString("en-US", { timeZone: local.timezone, hour: '2-digit', minute: '2-digit', hour12: state.twelveHourFormat })}
             </h1>
             <span class="text-neutral-500 dark:text-neutral-400 text-sm font-[Bungee]">{props.date.toLocaleDateString("en-US", { timeZone: local.timezone })}</span>
 
